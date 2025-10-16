d0ba03a17c7574e39a8ab0ee1872c84fad4c6d2e
ferdinand542025-stack <ferdinand54.2025@student.uny.ac.id>
2025-10-16 11:22:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,7 @@ with st.expander('Data'):
   y_raw = df.species
   y_raw
 
+'''
 with st.expander('Data visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
 
@@ -119,3 +120,4 @@ st.dataframe(df_prediction_proba,
 
 penguins_species = np.array(['Adelie', 'Chinstrap', 'Gentoo'])
 st.success(str(penguins_species[prediction][0]))
+'''
